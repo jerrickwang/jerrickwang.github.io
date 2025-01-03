74711d46605aa0b71d46a79c5d7be912c23d1b48
jerrickwang <jerrickwang20@gmail.com>
2025-01-03 13:52:17 -0800

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -15,3 +15,27 @@ function closeModal() {
   var modal = document.getElementById("imageModal");
   modal.style.display = "none"; // Hide the modal when clicked anywhere
 }
+// Function to update the date and time
+function updateDateTime() {
+  const now = new Date();
+  const options = {
+    weekday: 'long',
+    year: 'numeric',
+    month: 'long',
+    day: 'numeric',
+    hour: '2-digit',
+    minute: '2-digit',
+    second: '2-digit',
+    hour12: true,
+  };
+  const dateTimeString = now.toLocaleDateString('en-US', options);
+
+  // Set the current date and time to the #date-time element
+  document.getElementById('date-time').textContent = dateTimeString;
+}
+
+// Update date and time every second
+setInterval(updateDateTime, 1000);
+
+// Call once to set the initial value
+updateDateTime();
