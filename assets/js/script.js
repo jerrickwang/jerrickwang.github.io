c8af78f7ef99fda475816e0a180d40aeac5f417a
jerrickwang <jerrickwang20@gmail.com>
2025-01-07 00:23:29 -0800

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,32 +1,26 @@
-// Function to open the modal
 function openModal(imageElement) {
-  console.log('Modal is opening'); // Log to console for debugging
+  console.log('Modal is opening'); 
 
   var modal = document.getElementById("imageModal");
   var modalImage = document.getElementById("modalImage");
   
-  // Check if modal and image elements exist
   if (modal && modalImage) {
-    // Set the source of the modal image to the clicked image
-    modal.style.display = "block"; // Show the modal
-    modalImage.src = imageElement.src; // Set the clicked image as the modal image
+    modal.style.display = "block";
+    modalImage.src = imageElement.src;
   } else {
     console.error('Modal or Modal Image element not found!');
   }
 }
 
-// Function to close the modal when clicking anywhere (including the fullscreen image)
 function closeModal(event) {
   var modal = document.getElementById("imageModal");
   var modalImage = document.getElementById("modalImage");
 
-  // Close the modal when the user clicks anywhere on the screen, including the fullscreen image
   if (event.target === modal || event.target === modalImage || event.target === document.getElementById("close")) {
-    modal.style.display = "none"; // Hide the modal when clicked anywhere outside the image or on the close button
+    modal.style.display = "none";
   }
 }
 
-// Event listener to close the modal when clicking anywhere on the screen
 var modal = document.getElementById("imageModal");
 if (modal) {
   modal.addEventListener("click", closeModal);
